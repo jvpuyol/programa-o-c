da7a9a7532194b9cc3b70b1486a53a19ee633be5
jvpuyol <jvorrico10@gmail.com>
2025-08-03 14:34:55 -0300

    testando variavel.c

--- testando variavel.c
+++ testando variavel.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+    int idade = 24;
+    int quantidade = 1;
+    float altura = 1.71;
+    double peso= 90;
+    char letra = 'J';
+    char nome[10] = "joao";
+
+
+}
