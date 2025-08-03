8aa4ad126925f1ce920f149008c4c6479fb1cfed
jvpuyol <jvorrico10@gmail.com>
2025-08-03 15:37:58 -0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(){
+  int idade = 24;
+  float altura = 1.70;
+  double peso = 90;
+  char nome[10] = "Joao";
+
+  printf("Idade: %d anos\n", idade);
+  printf("Altura: %.2f metros\n", altura);
+  printf("Peso: %.1f kilos\n", peso);
+  printf("Nome: %s\n", nome);
+
+}
