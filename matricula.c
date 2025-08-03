3d92a2236b996c1758ffe8eaeaad38bb2206d60f
jvpuyol <jvorrico10@gmail.com>
2025-08-03 16:33:18 -0300

    Update matricula.c

--- matricula.c
+++ matricula.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[30];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    
+    printf("Digite seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("Nome do Aluno: %s - Matrícula: %d \n", nome, matricula);
+    printf("Altura: %.2f - idade: %d", idade, altura);
+
+    return 0;
+
 
